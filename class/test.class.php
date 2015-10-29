ab3e2ac08c08a408eb6c8d38cedb2a2a53af5a71
Frédéric Klee <git@fklee.name>
2015-10-29 14:30:06 +0100

    Initial commit

---
+++ class/test.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of test
+ *
+ * @author fklee
+ */
+class test {
+    public function __construct($a) {
+        echo "class construction ".$a."</br>";
+    }
+    
+    public function sayMyName($name) {
+        echo "this is my Name : ".$name."</br> " ;
+    }
+}
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+include "class/test.class.php" ;
+
+echo "ceci est un test pour git</br>" ;
+
+$test = new test(1) ;
+
+$test->sayMyName("toto");
+
